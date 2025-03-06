79aaa60c1ee82339212b6d7f9cc7a70b1ec75ad9
Nandini Gupta <nandinig.inbox@gmail.com>
2025-03-06 09:10:55 -0500

    adjusted instructions for medical filter (prompts)

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Co-Create Connect`;
 export const MESSAGE_PLACEHOLDER: string = `How can I support you today?`;
 export const FOOTER_MESSAGE: string = `Customized by Nandini Gupta`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat history`;
-export const PAGE_TITLE: string = `Co`;
+export const PAGE_TITLE: string = `Co-Create Connect`;
 export const PAGE_DESCRIPTION: string = `Get support from ${AI_NAME}, ${OWNER_NAME}'s assistant as you move towards your health goals!`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
