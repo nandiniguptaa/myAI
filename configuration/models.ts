50f803d67438c1ccbf0ce5eaea7bd2f2ed905201
Nandini Gupta <nandinig.inbox@gmail.com>
2025-03-17 23:42:07 -0400

    fine tuned model

--- configuration/models.ts
+++ configuration/models.ts
@@ -1,35 +1,35 @@
 import { ProviderName } from "@/types";
 
-export const INTENTION_MODEL: string = "gpt-4o-mini";
+export const INTENTION_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const INTENTION_TEMPERATURE: number = 0.7;
 
 export const MEDICAL_RESPONSE_PROVIDER: ProviderName = "openai";
-export const MEDICAL_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const MEDICAL_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const MEDICAL_RESPONSE_TEMPERATURE: number = 0.5;
 
 export const LABWORK_RESPONSE_PROVIDER: ProviderName = "openai";
-export const LABWORK_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const LABWORK_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const LABWORK_RESPONSE_TEMPERATURE: number = 0.5;
 
 export const EXERCISE_RESPONSE_PROVIDER: ProviderName = "openai";
-export const EXERCISE_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const EXERCISE_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const EXERCISE_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const NUTRITION_RESPONSE_PROVIDER: ProviderName = "openai";
-export const NUTRITION_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const NUTRITION_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const NUTRITION_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const RANDOM_RESPONSE_PROVIDER: ProviderName = "openai";
-export const RANDOM_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const RANDOM_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const RANDOM_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const HOSTILE_RESPONSE_PROVIDER: ProviderName = "openai";
-export const HOSTILE_RESPONSE_MODEL: string = "gpt-4o-mini";
+export const HOSTILE_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const HOSTILE_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const QUESTION_RESPONSE_PROVIDER: ProviderName = "openai";
-export const QUESTION_RESPONSE_MODEL: string = "gpt-4o";
+export const QUESTION_RESPONSE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const QUESTION_RESPONSE_TEMPERATURE: number = 0.7;
 
-export const HYDE_MODEL: string = "gpt-4o-mini";
+export const HYDE_MODEL: string = "ft:gpt-4o-mini-2024-07-18:nanai::BCI1fAJw";
 export const HYDE_TEMPERATURE: number = 0.7;
